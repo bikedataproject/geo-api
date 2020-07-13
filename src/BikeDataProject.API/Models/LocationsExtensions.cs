177749f1b46db9708d22204aef25d8f888f1cd94
BertSchoovaerts <bert.schoovaerts@student.kuleuven.be>
2020-07-13 16:58:36 +0200

    beginning of toContribution method

--- src/BikeDataProject.API/Models/LocationsExtensions.cs
+++ src/BikeDataProject.API/Models/LocationsExtensions.cs
@@ -1,7 +1,16 @@
+using System.Collections.Generic;
+using BikeDataProject.API.Domain;
+using NetTopologySuite.IO;
+
 namespace BikeDataProject.API.Models
 {
     public static class LocationsExtensions
     {
-        
+        public static Contribution toContribution(this List<Location> locations)
+        {
+            var contribution = new Contribution();
+            contribution.PointsGeom = new PostGisWriter().Write();
+
+        }
     }
 }
